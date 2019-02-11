d095d10a3ba4b337eb1129853e265f6dce66f3b0
bit <bit@DESKTOP-74J9BGV>
2019-02-11 10:24:41 +0900

    2019-02-11

---
+++ WebContent/rectangle.js
@@ -0,0 +1,20 @@
+/*
+ * Rectangle Class(생성함수)
+ */
+var Rectangle = function(x1, y1, x2, y2, backgroundColor){
+	this.x1 = x1;
+	this.y1 = y1;
+	this.x2 = x2;
+	this.y2 = y2;
+	this.backgroundColor = backgroundColor;
+}
+Rectangle.prototype.show = function(){
+	document.write(
+		"<div style='position:absolute; " + 
+		"left:" + this.x1 + "px; " + 
+		"top:" + this.y1 + "px; " + 
+		"width:" + (this.x2-this.x1) + "px; " +
+		"height:" + (this.y2-this.y1) + "px; " + 
+		"background-color:" + this.backgroundColor +
+		"'></div>" );
+}
